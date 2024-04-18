5dc58fcfe933f89c6b24c40f478888bac97a8b49
Amadou-dot <amadou.sseck@gmail.com>
2024-04-17 21:11:44 -0600

    Refactor MenuBtnHandler to improve code readability and initialization of variables

--- client/src/handlers/MenuBtnHandler.ts
+++ client/src/handlers/MenuBtnHandler.ts
@@ -1,10 +1,15 @@
-const leftSection: HTMLElement = document.querySelector(
-  '.left-section'
-) as HTMLElement;
-let isMenuOpen = leftSection.style.left === '0px';
+let leftSection: HTMLElement;
+let isMenuOpen = false;
+
+window.onload = () => {
+  leftSection = document.querySelector('.left-section') as HTMLElement;
+  isMenuOpen = leftSection.style.left === '0px';
+};
+
 export default function MenuBtnHandler(): void {
+  if (!leftSection) return;
+
   if (isMenuOpen) leftSection.style.left = '-160px';
   else leftSection.style.left = '0';
-
   isMenuOpen = !isMenuOpen;
 }
